053236da7ba19b49edd5079d5136bcd1d822fd17
Rafał Salwa <pirat1690@gmail.com>
2017-04-07 23:57:59 +0200

    feat(ApiBundle): Initial commit with struct for new Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new Sonata\AdminBundle\SonataAdminBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle(),
+            new ApiBundle\ApiBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
