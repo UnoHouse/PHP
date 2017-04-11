f1776bb4412ea17761ec1f2b823b7e59596eb89a
Rafał Salwa <pirat1690@gmail.com>
2017-04-11 22:11:34 +0200

    fix(authenticateController): added missing dependecies

--- src/ApiAndroidBundle/Controller/AuthenticationController.php
+++ src/ApiAndroidBundle/Controller/AuthenticationController.php
@@ -4,6 +4,7 @@ namespace ApiAndroidBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
 
 class AuthenticationController extends FOSRestController
 {
