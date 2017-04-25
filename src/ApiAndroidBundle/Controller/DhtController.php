3dbc6cfc44cb2128ae1e6cd740c94cf8d6322dd4
Rafał Salwa <pirat1690@gmail.com>
2017-04-25 16:34:00 +0200

    feat(dht11)controller for api

---
+++ src/ApiAndroidBundle/Controller/DhtController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ApiAndroidBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
+use CoreBundle\Entity;
+use CoreBundle\Constants;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
+class DhtController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/dht/temp-and-hum")
+     * @Rest\View()
+     *
+     */
+    public function getTempAndHumidityAction()
+    {
+        return new Response(
+            'No application data found',
+            Response::HTTP_NOT_FOUND,
+            array('Content-type' => 'application/json')
+        );
+    }
+
+    /**
+     * @Rest\Post("/dht/temp-and-hum")
+     * @Rest\View()
+     *
+     */
+    public function postTempAndHumidityAction()
+    {
+        dump($this->container->get('request'));exit;
+    }
+}
